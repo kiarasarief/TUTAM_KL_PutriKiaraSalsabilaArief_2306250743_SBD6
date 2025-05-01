0a3babc1eb31363cd0a42c812fa260a584f53e68
Kiara Arief <putrikiaaara@gmail.com>
2025-05-01 16:06:36 +0700

    update cors

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const cors = require('cors');  
-require('dotenv').config();
-
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-    origin: "https://os.netlabdte.com",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.json({ test: "Welcome to the API" });
 });
 
-app.use('/store', require('./src/routes/store.route'));
-app.use('/user', require('./src/routes/user.route'));
-app.use('/item', require('./src/routes/item.route'));
-app.use('/transaction', require('./src/routes/transaction.route'));
+app.use("/store", require("./src/routes/store.route"));
+app.use("/user", require("./src/routes/user.route"));
+app.use("/item", require("./src/routes/item.route"));
+app.use("/transaction", require("./src/routes/transaction.route"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
